5119a471d0a1471998f7d43e825106e8a3d1bcf8
WVillanuevaA <102330558+WVillanuevaA@users.noreply.github.com>
2024-03-30 12:58:37 -0500

    Add files via upload

---
+++ Lab03-DAEA/Lab03-DAEA/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab03_DAEA
+
+ {
+    public class DataAccess
+    {
+        private string connectionString = "LAB1504-31\\SQLEXPRESS ";
+        public DataTable GetStudentsConnected()
+        {
+            DataTable dataTable = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT StudentId, FirstName, LastName FROM Students";
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                adapter.Fill(dataTable);
+            }
+
+            return dataTable;
+        }
+        public List<Student> GetStudentsDisconnected()
+        {
+            List<Student> students = new List<Student>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT StudentId, FirstName, LastName FROM Students";
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Student student = new Student
+                    {
+                        StudentId = Convert.ToInt32(reader["StudentId"]),
+                        FirstName = reader["FirstName"].ToString(),
+                        LastName = reader["LastName"].ToString()
+                    };
+                    students.Add(student);
+                }
+            }
+
+            return students;
+        }
+    }
+
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
